88a65179436735c0290f7476fe366dbc5af4a824
Ravi1548 <96161226+Ravi1548@users.noreply.github.com>
2022-02-15 19:04:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
-
-
 let calculate = document.querySelector('#calculate');
 
 calculate.addEventListener('click', () => {
-    let totalBill = document.querySelector('#totalBill');
-    let tipPercent = document.querySelector('#tipPercent');
-    let split = document.querySelector('#split');
+    let totalBill = document.querySelector('#totalBill').value;
+    let tipPercent = document.querySelector('#tipPercent').value;
+    let split = document.querySelector('#split').value;
 
     //validate
-    if(totalBill.value === '' || tipPercent.value == 0 || split.value == 0){
-        alert('Please enter all values');
+    if(totalBill === ''|| split == 0){
+        alert('Please enter values');
         return;
     }
+  
+    
     //calculate
-    let total = (totalBill.value * tipPercent.value) / split.value;
+    let total = (totalBill * tipPercent) / split;
     total = total.toFixed(2);
     
     document.getElementById('tip').innerHTML = total;
 
-    //for getting the full amount
+  
 
-     let totalamount = (parseInt(totalBill.value)/(split.value)+ + parseInt(total));
-    document.getElementById('amount').innerHTML= totalamount;
-    totalamount = totalamount.toFixed(2);
 })
  
 
